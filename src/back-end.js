ef1e86d1e8141eeab1ff1358998015f8371c6b38
Aurora Shido-Wagenet and Chris Breaux <aurora@>
2019-03-11 16:26:14 -0700

    add ui, fix errors, add restart logic, clean out linter warnings

--- src/back-end.js
+++ src/back-end.js
@@ -3,35 +3,21 @@ export function Simon(round, memory) {
   this.memory = memory;
 }
 
-// Simon.prototype.pattern = function(){
-//  for the length of this.memory{
-//    set interval: 500?{
-//      if this is 1; flash button 1
-//      ect.
-//    }
-//   }
-// }
-
-// Simon.prototype.pattern = function(){
-//   var numTest = 0;
-//   function test() {
-//     console.log('test' + numTest);
-//     numTest++;
-//   }
-//
-//  for (var i = 0; i < this.memory.length; i++) {
-//   setTimeout(test, 1000);
-//   }
-// };
 
 Simon.prototype.pattern = function(){
-  var memory = this.memory
+  var memory = this.memory;
   for(var i = 0; i < this.memory.length; i++){
-      (function(memory){
-          setTimeout(function(){
-              console.log(memory);
-          }, 1000 * (i + 1));
-      })(memory[i]);
+    (function(memory){
+      setTimeout(function(){
+        $("#" + memory).css("opacity", ".5");
+        $("#" + memory)
+          .delay(800)
+          .queue(function (next) {
+            $(this).css('opacity', 'initial');
+            next();
+          });
+      }, 900 * (i + 1));
+    })(memory[i]);
   }
 };
 
@@ -40,8 +26,6 @@ Simon.prototype.turn = function() {
   this.memory.push(number);
   this.round++;
   this.pattern();
-  console.log(this.memory);
-  console.log(`The round is ${this.round}`);
 };
 
 export function Player(name, inputs) {
@@ -50,21 +34,24 @@ export function Player(name, inputs) {
 }
 
 export function checkWin(Simon, Player) {
-    for (var i=0; i<Player.inputs.length; i++){
-        if (Simon.memory[i] == Player.inputs[i]){
-          Player.win = true;
-        } else {
-          Player.win = false;
-        }
-        console.log(Player.win);
-      if (Player.win === false){
-        console.log("You Lose!");
-        return;
-      }
+  for (var i=0; i<Player.inputs.length; i++){
+    if (Simon.memory[i] == Player.inputs[i]){
+      Player.win = true;
+    } else {
+      Player.win = false;
     }
+    if (Player.win === false){
+      $("#start").hide();
+      $(".container").hide();
+      $("#lose").show();
+      $("#lastScore").text(`Your last score was ${Simon.round}`);
+      Simon.memory = [];
+      Simon.round = 0;
+      return;
+    }
+  }
   if (Player.inputs.length == Simon.memory.length){
     if (Player.win === true){
-      console.log("Next Round!");
       Player.inputs = [];
       Simon.turn();
     }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import './styles.css';
-import {round} from './back-end.js';
 import {Simon} from './back-end.js';
 import {Player} from './back-end.js';
 import {checkWin} from './back-end.js';
@@ -9,12 +8,20 @@ $(document).ready(function() {
   var game = new Simon(0, []);
   var player1 = new Player('Player 1', []);
 
-    $(".numberButtons").click(function(){
-      var currentNumber = ($(this).text());
-      console.log(currentNumber);
-      player1.inputs.push(currentNumber);
-      console.log(player1.inputs);
-      checkWin(game, player1);
-    });
+  $("#start").click(function(){
+    $("#start").hide();
     game.turn();
+  });
+
+  $(".numberButtons").click(function(){
+    var currentNumber = ($(this).text());
+    player1.inputs.push(currentNumber);
+    checkWin(game, player1);
+  });
+
+  $("#startOver").click(function(){
+    $("#lose").hide();
+    $("#start").show();
+    $(".container").show();
+  });
 });

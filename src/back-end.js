c926978fcce24238f58d3a5445ce026a2f7cc8f1
Aurora Shido-Wagenet and Chris Breaux <aurora@>
2019-03-11 14:54:57 -0700

    add pattern function, make game fully playable with console logs

--- src/back-end.js
+++ src/back-end.js
@@ -3,10 +3,44 @@ export function Simon(round, memory) {
   this.memory = memory;
 }
 
+// Simon.prototype.pattern = function(){
+//  for the length of this.memory{
+//    set interval: 500?{
+//      if this is 1; flash button 1
+//      ect.
+//    }
+//   }
+// }
+
+// Simon.prototype.pattern = function(){
+//   var numTest = 0;
+//   function test() {
+//     console.log('test' + numTest);
+//     numTest++;
+//   }
+//
+//  for (var i = 0; i < this.memory.length; i++) {
+//   setTimeout(test, 1000);
+//   }
+// };
+
+Simon.prototype.pattern = function(){
+  var memory = this.memory
+  for(var i = 0; i < this.memory.length; i++){
+      (function(memory){
+          setTimeout(function(){
+              console.log(memory);
+          }, 1000 * (i + 1));
+      })(memory[i]);
+  }
+};
+
 Simon.prototype.turn = function() {
   var number = Math.floor(Math.random() * 4) + 1;
   this.memory.push(number);
   this.round++;
+  this.pattern();
+  console.log(this.memory);
   console.log(`The round is ${this.round}`);
 };
 
@@ -15,44 +49,24 @@ export function Player(name, inputs) {
   this.inputs = inputs;
 }
 
-Player.prototype.play = function(Simon) {
-  this.inputs = [];
-  var memoryLength = Simon.memory.length;
-  // while (this.inputs.length < memoryLength){
-  //
-  // }
-  // var answer = parseInt(prompt('Enter numbers:'));
-  this.inputs.push();
-  console.log();
-}
-
-// export function round(Simon, Player){
-//   Simon.turn();
-//   console.log(Simon.memory);
-//   Player.play(Simon);
-//   console.log(Player.inputs);
-//   var winning = checkWin(Simon, Player);
-//   console.log(winning);
-//   if (winning == false){
-//     console.log("You Lose!");
-//   } else {
-//     console.log(`Next round!`);
-//     round(Simon, Player);
-//   }
-// }
-
-
 export function checkWin(Simon, Player) {
-  for (var i=0; i<Player.inputs.length; i++){
-    if (Simon.memory[i] === Player.inputs[i]){
-      return true;
-    } else {
-      return false;
+    for (var i=0; i<Player.inputs.length; i++){
+        if (Simon.memory[i] == Player.inputs[i]){
+          Player.win = true;
+        } else {
+          Player.win = false;
+        }
+        console.log(Player.win);
+      if (Player.win === false){
+        console.log("You Lose!");
+        return;
+      }
+    }
+  if (Player.inputs.length == Simon.memory.length){
+    if (Player.win === true){
+      console.log("Next Round!");
+      Player.inputs = [];
+      Simon.turn();
     }
   }
-  // if (Simon.memory[0] == Player.inputs[0]) {
-  //   console.log("winning");
-  // } else {
-  //   console.log("not winning");
-  // }
 }
--- src/main.js
+++ src/main.js
@@ -9,16 +9,12 @@ $(document).ready(function() {
   var game = new Simon(0, []);
   var player1 = new Player('Player 1', []);
 
-  $(".numberButtons").click(function(){
-    var currentNumber = ($(this).text());
-    console.log(currentNumber);
-  });
-
-  // round(game, player1);
-
-  // game.turn();
-  //
-  //
-  // player1.play();
-  // checkWin(game,player1);
+    $(".numberButtons").click(function(){
+      var currentNumber = ($(this).text());
+      console.log(currentNumber);
+      player1.inputs.push(currentNumber);
+      console.log(player1.inputs);
+      checkWin(game, player1);
+    });
+    game.turn();
 });
